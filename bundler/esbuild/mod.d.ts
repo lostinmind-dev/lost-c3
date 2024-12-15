e21be29860ebf6284e9766255d6887ce25e61dc4
Nikita <llostinmind@gmail.com>
2024-12-15 12:17:09 +0500

    .

--- bundler/esbuild/mod.d.ts
+++ bundler/esbuild/mod.d.ts
@@ -695,11 +695,11 @@ export declare function stop(): Promise<void>
 //
 //   https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/826
 //
-declare global {
-  namespace WebAssembly {
-    interface Module {
-    }
-  }
-  interface URL {
-  }
-}
+// declare global {
+//   namespace WebAssembly {
+//     interface Module {
+//     }
+//   }
+//   interface URL {
+//   }
+// }
